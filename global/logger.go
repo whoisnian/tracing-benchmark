e5aab4f8f5b7e99805db4f5fd0ed9fda00ce8bfa
whoisnian <zhuchangbao1998@gmail.com>
2024-09-14 02:09:46 +0800

    ADD: (server) slog.Logger with trace info

--- global/logger.go
+++ global/logger.go
@@ -1,15 +1,49 @@
 package global
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"go.elastic.co/apm/v2"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var LOG *slog.Logger
 
 func SetupLogger() {
-	LOG = slog.New(slog.NewJSONHandler(
-		os.Stderr,
-		&slog.HandlerOptions{AddSource: true, Level: slog.LevelInfo},
-	))
+	LOG = slog.New(&TraceHandler{
+		slog.NewJSONHandler(
+			os.Stderr,
+			&slog.HandlerOptions{AddSource: true, Level: slog.LevelInfo},
+		),
+	})
+}
+
+type TraceHandler struct{ slog.Handler }
+
+func (h *TraceHandler) Handle(ctx context.Context, r slog.Record) error {
+	var (
+		traceID = "unknown"
+		spanID  = "unknown"
+	)
+	switch CFG.TraceBackend {
+	case "otlp":
+		sc := trace.SpanFromContext(ctx).SpanContext()
+		traceID = sc.TraceID().String()
+		spanID = sc.SpanID().String()
+	case "apm":
+		tc := apm.SpanFromContext(ctx).TraceContext()
+		traceID = tc.Trace.String()
+		spanID = tc.Span.String()
+	}
+
+	if CFG.TraceBackend != "none" {
+		r.AddAttrs(
+			slog.String("trace_id", traceID),
+			slog.String("span_id", spanID),
+		)
+	}
+
+	return h.Handler.Handle(ctx, r)
 }
