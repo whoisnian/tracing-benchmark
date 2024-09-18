0ff1032066e536b80b7e2e713dc55385b4b2f2d2
whoisnian <zhuchangbao1998@gmail.com>
2024-09-19 03:33:16 +0800

    ADD: (server) transaction.id for apm

--- global/logger.go
+++ global/logger.go
@@ -24,26 +24,34 @@ type TraceHandler struct{ slog.Handler }
 
 func (h *TraceHandler) Handle(ctx context.Context, r slog.Record) error {
 	var (
-		traceID = "unknown"
-		spanID  = "unknown"
+		traceID       = "unknown"
+		spanID        = "unknown"
+		transactionID = "unknown"
 	)
 	switch CFG.TraceBackend {
 	case "otlp":
-		if sc := trace.SpanFromContext(ctx).SpanContext(); sc.IsValid() {
+		// https://github.com/open-telemetry/opentelemetry-go/blob/ed4fc757583a88b4da51b1fe1c3f0703ac27a487/sdk/log/logger.go#L73
+		if sc := trace.SpanContextFromContext(ctx); sc.IsValid() {
 			traceID = sc.TraceID().String()
 			spanID = sc.SpanID().String()
 		}
-	case "apm":
-		if tc := apm.SpanFromContext(ctx).TraceContext(); tc.Trace.Validate() == nil && tc.Span.Validate() == nil {
-			traceID = tc.Trace.String()
-			spanID = tc.Span.String()
-		}
-	}
-
-	if CFG.TraceBackend != "none" {
 		r.AddAttrs(
 			slog.String("trace_id", traceID),
 			slog.String("span_id", spanID),
+		)
+	case "apm":
+		// https://github.com/elastic/apm-agent-go/blob/096f5c06b782ae2b7c59d9eb4092a63a9a1886bd/module/apmzap/fields.go#L42
+		if tx := apm.TransactionFromContext(ctx); tx != nil {
+			traceID = tx.TraceContext().Trace.String()
+			transactionID = tx.TraceContext().Span.String()
+			if span := apm.SpanFromContext(ctx); span != nil {
+				spanID = span.TraceContext().Span.String()
+			}
+		}
+		r.AddAttrs(
+			slog.String("trace.id", traceID),
+			slog.String("transaction.id", transactionID),
+			slog.String("span.id", spanID),
 		)
 	}
 
